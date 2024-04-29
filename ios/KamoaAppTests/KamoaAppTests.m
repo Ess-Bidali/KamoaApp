f25c348da68d417242a3f22c7e1958c2804f76dd
Ess-Bidali <essiebidali@gmail.com>
2024-04-29 16:56:28 +0300

    feat(app): fix app name typo

--- ios/KamoaAppTests/KamoaAppTests.m
+++ ios/KamoaAppTests/KamoaAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface KamuaAppTests : XCTestCase
+@interface KamoaAppTests : XCTestCase
 
 @end
 
-@implementation KamuaAppTests
+@implementation KamoaAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
