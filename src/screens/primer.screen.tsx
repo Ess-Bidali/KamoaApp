65124106c1d0bb79fb8710998223e4fc1d296f68
Ess-Bidali <essiebidali@gmail.com>
2024-04-29 10:13:19 +0300

    feat(primer): add placeholder screen

---
+++ src/screens/primer.screen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import PageWrapper from '../components/page-wrapper';
+
+function PrimerScreen(): React.JSX.Element {
+  return (
+    <PageWrapper>
+      <View>
+        <Text>TEST</Text>
+      </View>
+    </PageWrapper>
+  );
+}
+
+export default PrimerScreen;
